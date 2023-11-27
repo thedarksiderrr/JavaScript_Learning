6ae26197e79ecf8f34b426f8e610c962a6416e9a
Pratham Chapla <prathamchapla789@gmail.com>
2023-11-27 13:45:07 +0000

    Conversion operations added

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,84 @@
+// let score = "33dsdsd"
+// let score1 = null
+// let score2 = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score)); //as a funtion
+
+// let valuesInNumber = Number(score1); // convert string (number) to number datatype
+// console.log(typeof valuesInNumber);
+// console.log(valuesInNumber); //NaN - not a number
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1
+false => 0
+*/
+
+// let isLoggedIn = 1; //true
+// let isLoggedIn = 0; //false
+
+// let isLoggedIn = "abc"; //true
+// let isLoggedIn = ""; //empty string will be false
+
+// let isBooleanLoggedIn = Boolean(isLoggedIn);
+// console.log(isBooleanLoggedIn);
+
+/*
+0 => false
+1 => true
+"" => false
+"abc" => true
+*/
+
+// let num = 33
+
+// let str = String(num)
+// console.log(str);
+// console.log(typeof str);
+
+// ************************ Operations ************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //2*2*2
+// console.log(2/2);
+// console.log(2%2);
+
+// *** concetinate *** 
+
+let str1 = "Pratham"
+let str2 = " Chapla"
+let str3 = str1 + str2
+// console.log(str3);
+
+// ************************
+
+// console.log(1 + "2"); //12
+// console.log("1" + 2); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log( (3 + 4) * 2 % 3); //5
+
+// console.log(+true); //1
+// console.log(+""); //0 - false
+
+let gameCounter = 100
+gameCounter++; //postfix
+++gameCounter; //prefix
+console.log(gameCounter);
+
+// link to study increments operations 
+
+/*
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+*/

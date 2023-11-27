125de66902641b7a00fb746cb9a51a76c652a821
Pratham Chapla <prathamchapla789@gmail.com>
2023-11-27 11:02:30 +0000

    variable and datatypes added

---
+++ 01_basics/01_Variable.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "pratham@gmail.com"
+var accountPassword = "123"
+accountCity = "Junagadh"
+
+// accountId = 6789 //- changes are not allowed
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //trat all JS code as newer variables.
+
+// alert(3+3); // in nodejs it doesn't support, it supports only on browser
+
+//primitve datatype
+
+let num = 123; //size 2 to power 53
+let bInt = 65655765567; // size > number datatypes's length then use bigint
+let str = "Pratham Chapla"; //print any statement (use "" or else '')
+let bool = true; //true or false (boolean)
+let n = null; //standalone value (neither o  or empty)
+let ud = undefined; //its value is not defined
+
+//symbol => unique value.
+
+// non-primitive datatype
+
+// Object
+
+//how get to know datatype of any value.
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+console.log(typeof str); //number
